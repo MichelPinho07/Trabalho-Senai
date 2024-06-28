db6bc3b3fd81832c8d3eafa42ec271b50d595d4a
MichelPinho07 <michelpinho7k@gmail.com>
2024-06-27 21:26:15 -0300

    Trabalho atualizado 27/06

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-let filmes =[]
+let filmes = []
 const div = document.querySelector(`#lista-filme`)
-function enviarFilme(){
+function enviarFilme() {
     const dadosFilme = []
     const form = document.querySelector(`form`)
     const input = new FormData(form)
-    
+
 
     const foto = input.get(`foto`)
     const titulo = input.get(`titulo`)
@@ -29,12 +29,10 @@ function enviarFilme(){
     <p>${dadosFilme[6]}</p>
 </div>`
 }
-let filmesArmazenados = localStorage.getItem(`filmes`)
-if(filmesArmazenados){
-    filmes = JSON.parse(filmesArmazenados)
-
-    for(const dadosFilme of filmes){
-        div.innerHTML += `<div>
+function dados(){
+    let contagem = 0
+    for (const dadosFilme of filmes) {
+        div.innerHTML += `<div>]
         <img src="${dadosFilme[0]}" alt="">
         <h2>${dadosFilme[1]}</h2>
         <p>${dadosFilme[2]}</p>
@@ -42,8 +40,25 @@ if(filmesArmazenados){
         <p>${dadosFilme[4]}</p>
         <p>${dadosFilme[5]}</p>
         <p>${dadosFilme[6]}</p>
+        <button onclick="excluir(${contagem})">Excluir</button>
     </div>`
-     
+        contagem++
     }
 }
+let filmesArmazenados = localStorage.getItem(`filmes`)
+if (filmesArmazenados) {
+    filmes = JSON.parse(filmesArmazenados)
+    
+    dados()
+}
+function excluir(indice){
+    filmes.splice(indice, 1)
+    localStorage.setItem(`filmes`, JSON.stringify(filmes))
+    div.innerHTML = ``
+    dados()
+}
 
+function ocultar(){
+    const form = document.querySelector(`form`)
+    form.classList.toggle(`oculto`)
+}
